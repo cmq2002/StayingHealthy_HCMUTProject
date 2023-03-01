89cf91b2abbeeb9464858eefe55b476ecad48db1
Cao Minh Quang <quang.cao2205@gmail.com>
2023-03-01 13:34:50 +0700

    update data format

--- data.js
+++ data.js
@@ -3,21 +3,24 @@ let recipes = [
     title: "Dish 1",
     intro: "this is dish 1",
     ingr: "egg, tomato",
-    instr: "B1:....<br>B2:.....<br>B3:......",
+    instr: ["b1", "b2", "b3"],
     img: "img/gallery/04.jpg",
+    calo: 29,
     nutri: "calo:, fatL, cab:, sugar"},
     
     {id: 2,
     title: "Dish 2",
     intro: "this is dish 1",
     ingr: "egg, tomato",
-    instr: "B1:....<br>B2:.....<br>B3:......",
+    instr: ["b1", "b2", "b3"],
+    calo: 32,
     nutri: "calo:, fatL, cab:, sugar"},
 
     {id: 3,
     title: "Dish 3",
     intro: "this is dish 1",
     ingr: "egg, tomato",
-    instr: "B1:....<br>B2:.....<br>B3:......",
+    instr: ["b1", "b2", "b3"],
+    calo: 34,
     nutri: "calo:, fatL, cab:, sugar"}
 ]
