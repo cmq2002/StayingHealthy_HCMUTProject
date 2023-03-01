736dffa0bfd803be221e323f91ff9aee4f4a2af8
Cao Minh Quang <quang.cao2205@gmail.com>
2023-03-01 13:02:32 +0700

    update db

---
+++ data.js
@@ -0,0 +1,23 @@
+let recipes = [
+    {id: 1,
+    title: "Dish 1",
+    intro: "this is dish 1",
+    ingr: "egg, tomato",
+    instr: "B1:....<br>B2:.....<br>B3:......",
+    img: "img/gallery/04.jpg",
+    nutri: "calo:, fatL, cab:, sugar"},
+    
+    {id: 2,
+    title: "Dish 2",
+    intro: "this is dish 1",
+    ingr: "egg, tomato",
+    instr: "B1:....<br>B2:.....<br>B3:......",
+    nutri: "calo:, fatL, cab:, sugar"},
+
+    {id: 3,
+    title: "Dish 3",
+    intro: "this is dish 1",
+    ingr: "egg, tomato",
+    instr: "B1:....<br>B2:.....<br>B3:......",
+    nutri: "calo:, fatL, cab:, sugar"}
+]
